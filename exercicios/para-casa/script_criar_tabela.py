511fa4288d7f0ff4187a545294cf7e974126c0bf
Jardielen Chaves <jardielen08@gmail.com>
2024-05-22 18:37:06 -0300

    adicionando respostas do exercicio da semana 8

---
+++ exercicios/para-casa/script_criar_tabela.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('livraria.db')
+cursor = conn.cursor()
+
+cursor.execute("""
+            CREATE TABLE IF NOT EXISTS livros (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            autor TEXT,
+            ano INTEGER,
+            preco REAL
+        );
+""")
+
+conn.commit()
+cursor.close()
+conn.close()
