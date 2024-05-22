65f0b2fbfa8c662c7967ef09923c1dc6bf06bbd1
Fefanez <fernanda.anez@gmail.com>
2024-05-22 10:32:16 -0300

    Atualização da atividade semanal.

--- exercicios/para-casa/exercicio_semanal/3_consulta.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('livraria.db')
-cursor = conn.cursor()
-
-cursor.execute('SELECT * FROM livros')
-registros = cursor.fetchall()
-for registro in registros:
-    print(registro)
